efe43d4c8b661e7e49c3465087b90a8278e85a50
유선우 <sunwoori@hotmail.com>
2022-10-06 11:36:16 +0900

    초기 커밋.

---
+++ RedisSample/Constant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleProject
+{
+    public static class Constant
+    {
+        public static string redis_ip = "132.226.224.188";
+        public static int redis_port = 6379;
+        public static string redis_pass = "redis";
+        public static string redis_default_db = "0";
+    }
+}
---
+++ RedisSample/RedisSample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Redis;
+
+namespace SampleProject
+{
+    public class RedisSample
+    {
+        private ConnectionMultiplexer _conntction;
+        public IDatabase redisDatabase;
+        public IServer redisServer;
+
+        public RedisSample(string host, int port, string pass, string db)
+        {
+            this._conntction = ConnectionMultiplexer.Connect(host + ":" + port + ",password=" + pass+ ",DefaultDatabase=" + db);
+            if (_conntction.IsConnected)
+            {
+                this.redisDatabase = this._conntction.GetDatabase();
+                this.redisServer = this._conntction.GetServer(host + ":" + port);
+            }
+        }
+    }
+}
