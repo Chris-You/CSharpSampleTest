f32af5e2ea70cac93419ac31e330fd0737316980
Chris-You <sunwoori@hotmail.com>
2022-10-06 13:10:41 +0900

    Update Config.cs
    config

--- RedisSample/Config.cs
+++ RedisSample/Config.cs
@@ -6,7 +6,7 @@ namespace SampleProject
 {
     public static class Config
     {
-        public static string redis_ip = "132.226.224.188";
+        public static string redis_ip = "000.000.000.000";
         public static string redis_port = "6379";
         public static string redis_pass = "redis";
         public static string redis_default_db = "0";
