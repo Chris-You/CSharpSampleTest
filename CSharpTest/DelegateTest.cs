b7a2c408411e1a83237927b455ea324b7b89f7c2
유선우 <sunwoori@hotmail.com>
2022-11-29 13:09:09 +0900

    델리게이트 테스트 코드

---
+++ CSharpTest/DelegateTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTest
+{
+    delegate void ResultHelper(Func<bool, bool> work, Action success, Action fail);
+
+    public class DelegateTest
+    {
+        public bool Work(bool isOk)
+        {
+            return isOk;
+        }
+
+        public void Success()
+        {
+            //return 0;
+            Console.WriteLine("success method");
+        }
+
+        public void Fail()
+        {
+            Console.WriteLine("fail method");
+        }
+
+        public void Result(Func<bool, bool> work, Action success, Action fail)
+        {
+            
+            if (work(true))
+            {
+                success();
+            }
+            else
+            {
+                fail();
+            }
+
+            if (work(false))
+            {
+                success();
+            }
+            else
+            {
+                fail();
+            }
+        }
+
+
+        public void Process()
+        {
+
+            ResultHelper result = new ResultHelper(Result);
+            Func<bool, bool> func = Work;
+            //func(false);
+            result(func, Success, Fail);
+        }
+
+
+    }
+}
