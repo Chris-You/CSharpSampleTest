4cde99e6cdfa5e7ae0ee81a498f06931b9028b48
유선우 <sunwoori@hotmail.com>
2022-11-29 13:07:59 +0900

    서비스 생명주기

---
+++ WebAPISampleNet5/Services/ServiceScoped.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPISampleNet5.Services
+{
+    public interface IServiceScoped
+    {
+
+    }
+
+    public class ServiceScoped : IServiceScoped
+    {
+
+    }
+}
---
+++ WebAPISampleNet5/Services/ServiceTransient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPISampleNet5.Services
+{
+    public interface IServiceTransient
+    {
+
+    }
+
+    public class ServiceTransient : IServiceTransient
+    {
+
+    }
+}
