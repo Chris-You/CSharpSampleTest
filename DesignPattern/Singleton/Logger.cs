da05991c0f5a295dc66ba76c44b72f3ba442f4f8
유선우 <sunwoori@hotmail.com>
2022-12-06 09:13:47 +0900

    싱글턴 패턴

---
+++ DesignPattern/Singleton/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    public class Logger
+    {
+        private static Logger logger;
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if(logger == null)
+            {
+                logger = new Logger();
+            }
+
+            return logger;
+        }
+    }
+}
